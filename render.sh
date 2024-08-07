ed334ce5e277e2c543811a45aa62e61be58164ee
emilio-berti <emilio.berti90@gmail.com>
2024-08-07 15:40:52 +0200

    :wrench: remove hard characters from EU.shp

--- render.sh
+++ render.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 source activate pkgdown
-R -e "bookdown::render_book()" > render.log
+R -e "bookdown::render_book()" > render-html.log
+R -e "bookdown::render_book(output_format = bookdown::pdf_book())" > render-pdf.log
 
